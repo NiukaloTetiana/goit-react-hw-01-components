5022641cd2694d99b42e205fd220cd3626009142
Niukalo Tetiana <tanusha.17121994@gmail.com>
2024-01-04 14:40:22 +0200

    reorganization

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import './index.css';
 import { ThemeProvider } from 'styled-components';
-import { theme } from 'components/utils/theme';
+import { theme } from 'components/styles/theme';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
